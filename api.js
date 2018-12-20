88f5a9125ae639c7e396be4e3e9b4e7ad7cc5f18
mooshoe <mooshoe@fastmail.com>
2018-12-20 15:45:10 -0600

    IM COMMITING

--- api.js
+++ api.js
@@ -18,8 +18,7 @@ function protect(req, res, next) {
 
 // routes
 router.post("/token", function(req, res) {
-  const statement = db.prepare("SELECT * FROM users WHERE username = ?");
-  const account = statement.get(req.body.username);
+  const account = db.prepare("SELECT * FROM users WHERE username = ?").get(req.body.username);
   if (account == undefined) {
     res.json({"error": "username"});
   } else if (req.body.password != account.password) {
@@ -58,7 +57,7 @@ router.get("/posts/:id", function(req, res) {
       },
       post_id: comment.post_id,
       comment: comment.content,
-      replies: replies,
+      replies: replies, 
       date_created: comment.date
     });
   }
@@ -108,6 +107,17 @@ router.post("/posts/:id/like", protect, function(req, res) {
   }
 });
 
+router.post("/posts/:id/unlike", protect, function(req, res) {
+  const post = db.prepare("SELECT * FROM posts WHERE id = ?").get(req.params.id);
+  if (post == undefined)
+    res.json({"error": "id"});
+  const like = db.prepare("SELECT * FROM likes WHERE user_id = ? and post_id = ?").get(req.account.id, req.params.id);
+  if (like == undefined)
+    res.json({"error": "not liked"});
+  db.prepare("DELETE FROM likes WHERE user_id = ? and post_id = ?").run(req.account.id, req.params.id);
+  res.json({"status": "ok"});
+});
+
 router.post("/posts/:id/comment", protect, function(req, res) {
   const post = db.prepare("SELECT * FROM posts WHERE id = ?").get(req.params.id);
   if (post == undefined) 
@@ -138,9 +148,8 @@ router.post("/posts/:id/reply", protect, function(req, res) {
 
 router.get("/timeline", function(req, res) {
   var response = [];
-  for (const post of db.prepare("SELECT * FROM posts ORDER BY date DESC LIMIT 10").all()) {
+  for (const post of db.prepare("SELECT * FROM posts ORDER BY date DESC LIMIT 20").all()) {
     const user = db.prepare("SELECT * FROM users WHERE id = ?").get(post.user_id);
-    
     var liked = [];
     for (const user_liked of db.prepare("SELECT * FROM likes WHERE post_id = ?").all(post.id)) {
       const user_here = db.prepare("SELECT * FROM users WHERE id = ?").get(user_liked.user_id);
@@ -149,13 +158,12 @@ router.get("/timeline", function(req, res) {
         username: user_here.username
       });
     }
-    
     response.push({    
       id: post.id,
       user: {
         id: user.id,
         username: user.username
-      }, 
+      },
       title: post.title,
       content: post.content,
       likes: liked,
@@ -171,4 +179,4 @@ module.exports = router;
 // also please get ids to be randomly generated 
 // ok lets test to see if this workflow well, works
 
-// DROP TABLE posts; DROP TABLE users; DROP TABLE likes; DROP TABLE comments; CREATE TABLE posts (id INT, title CHAR(10), content CHAR(3000), user_id INT, date CHAR(16)); CREATE TABLE users(id INT, username CHAR(10), password CHAR(16)); CREATE TABLE likes (post_id INT, user_id INT); CREATE TABLE comments (comment_id INT, post_id INT, content CHAR(3000), user_id INT, date CHAR(16)); CREATE TABLE replies (id INT, comment_id INT, content CHAR(500), user_id INT, date CHAR(30)); INSERT INTO users VALUES ('1', 'admin', 'password'); INSERT INTO users VALUES ('2', 'mooshoe', 'moocowspassword'); INSERT INTO posts VALUES ('1000000', 'First post!', '{content}', '1', '2018-12-09T17:09:47+00:00'); INSERT INTO likes VALUES ('1000000', '1'); INSERT INTO comments VALUES ('1', '1000000', '{comment}', '1', '2018-12-14T00:17:46+00:00'); INSERT INTO replies VALUES ('1', '1', '{reply}', '2', '2018-12-15T19:26:41+00:00');
+// CREATE TABLE posts (id INT, title CHAR(10), content CHAR(3000), user_id INT, date CHAR(16)); CREATE TABLE users(id INT, username CHAR(10), password CHAR(16)); CREATE TABLE likes (post_id INT, user_id INT); CREATE TABLE comments (comment_id INT, post_id INT, content CHAR(3000), user_id INT, date CHAR(16)); CREATE TABLE replies (id INT, comment_id INT, content CHAR(500), user_id INT, date CHAR(30)); INSERT INTO users VALUES ('1', 'admin', 'password'); INSERT INTO users VALUES ('2', 'mooshoe', 'moocowspassword'); INSERT INTO posts VALUES ('1000000', 'First post!', '{content}', '1', '2018-12-09T17:09:47+00:00'); INSERT INTO likes VALUES ('1000000', '1'); INSERT INTO comments VALUES ('1', '1000000', '{comment}', '1', '2018-12-14T00:17:46+00:00'); INSERT INTO replies VALUES ('1', '1', '{reply}', '2', '2018-12-15T19:26:41+00:00');
--- documentation.md
+++ documentation.md
@@ -1,14 +1,19 @@
-# skfs api/v1 Documentation
+# skfs v1 Documentation
 
 This is the REST API for skfs, which can be used to access the site to create bots and clients.
 
 ## Table of contents
+#### REST api
 * [POST /api/v1/token](#POST-/api/v1/token)
 * [GET /api/v1/timeline](#POST-/api/v1/timeline)
+* [GET /api/v1/posts/:id](#POST-/api/v1/:id)
+#### Entities
 
 ### POST /api/v1/token
 
-Returns a session token to the client, which can be access actions only users can do.
+A session token to the client, which can be access actions only users can do.
+
+Returns [Token](#Token)
 
 #### Resource Information
 
@@ -25,5 +30,121 @@ Returns a session token to the client, which can be access actions only users ca
 | username | The username of the user you want to log into. | Yes |
 | password | Password of the user you want to log into. | Yes |
 
-### GET /api/v1/timeline
+#### GET /api/v1/timeline
 
+This is the the firehose timeline of the instance, which contains the 20 most recent posts.
+
+Returns an array [Post](#Post)
+
+#### Resource Information
+
+| Infromation | ? |
+| - | - |
+| Response format | Json |
+| Requires token | No |
+| Available since | 0.0.0 |
+
+### GET /api/v1/posts/:id
+
+Gives you a post, when given an id.
+
+Returns [Post](#Post)
+
+#### Resource Information
+
+| Information | ? |
+|-|-|
+| Response format | Json |
+| Requires token | No |
+| Available since | 0.0.0 |
+
+### POST /api/v1/post
+
+Creates a post on skfs.
+
+#### Resource Information
+
+| Information | ? |
+|-|-|
+| Response format | Json |
+| Requires token | Yes |
+| Available since | 0.0.0 |
+
+#### Paramaters
+
+| Name | Description | Required |
+| - | - | - |
+| token | Your personal token | Yes |
+| title | Title of the post you are creating. | Yes |
+| content | The contents of the post. | Yes |
+
+### POST /api/v1/posts/:id/like
+
+Likes a post. You can only do this once. ;)
+
+#### Resource Information
+
+| Information | ? |
+|-|-|
+| Response format | Json |
+| Requires token | Yes |
+| Available since | 0.0.0 |
+
+#### Paramaters
+
+| Name | Description | Required |
+| - | - | - |
+| token | Your personal token | Yes |
+
+### POST /api/v1/posts/:id/unlike
+
+#### Resource Information
+
+| Information | ? |
+|-|-|
+| Response format | Json |
+| Requires token | Yes |
+| Available since | 0.0.0 |
+
+### POST /api/v1/post/:id/comment
+
+Comments on post.
+
+#### Resource Information
+
+| Information | ? |
+|-|-|
+| Response format | Json |
+| Requires token | Yes |
+| Available since | 0.0.0 |
+
+#### Paramaters
+
+| Name | Description | Required |
+| - | - | - |
+| token | Your personal token | Yes |
+| comment | Contents of your comment | Yes |
+
+### POST /api/v1/post/:id/reply
+
+Reply to a comment using this. You cannot reply to a reply, but replying to a comment mentioning someone in the reply works.
+
+#### Resource Information
+
+| Information | ? |
+|-|-|
+| Response format | Json |
+| Requires token | Yes |
+| Available since | 0.0.0 |
+
+#### Paramaters
+
+| Name | Description | Required |
+| - | - | - |
+| token | Your personal token | Yes |
+| reply | Contents of your comment | Yes |
+| comment_id | ID of the comment you want to reply to | Yes |
+
+## Entities
+
+### ima do this shit later
