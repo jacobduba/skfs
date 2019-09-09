0191adc57f43b811ffbb28382a7a86c54634b70a
jacob <jacob@mooshoe.me>
2019-09-09 17:41:31 +0000

    commits starting again, bless this class

--- server/router.js
+++ server/router.js
@@ -17,13 +17,13 @@ router.get('/login', function(req, res) {
     res.redirect('/');
   }
   else
-    res.render('login', { loginError: false });
+    res.render('login', { loginError: false, layout: false });
 });
 
 router.post('/login', function(req, res) {
   const account = db.prepare("SELECT * FROM users WHERE email = ?").get(req.body.email);
   if (account == undefined || !bcrypt.compareSync(req.body.password, account.password))
-    res.render('login', { loginError: true});
+    res.render('login', { loginError: true, layout: false });
   else {
     res.cookie("token", jwt.sign({ id: account.id }, process.env.SECRET));
     if (req.query['from'] == 'token')
@@ -37,7 +37,7 @@ router.get('/token', function(req, res) {
   if (req.cookies.token == undefined)
     res.redirect("/login?from=token");
   else
-    res.render('token', { token: req.cookies.token });
+    res.render('token', { token: req.cookies.token, layout: false });
 })
 
 router.get('/logout', function(req, res) {
