7fa062d02a32bc6976a71a2b6a1861784f938522
jacob <jacob@mooshoe.me>
2019-09-24 23:59:31 +0000

    last commit before client gone

--- client/src/PostSnippet.js
+++ client/src/PostSnippet.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Moment from 'react-moment';
 import User from './User.js';
 import Cookies from "universal-cookie";
-import { BrowserRouter, Route, Link} from 'react-router-dom';
+import { BrowserRouter, Route, Link } from 'react-router-dom';
 
 const cookies = new Cookies();
 
@@ -17,7 +17,7 @@ class PostSnippet extends React.Component {
   }
 
   componentDidMount() {
-    this.setState({ likeCounter: this.props.post.likes.length});
+    this.setState({ likeCounter: this.props.post.likes.length });
     if (this.props.user == undefined)
       return;
     for (var user of this.props.post.likes) {
@@ -38,12 +38,12 @@ class PostSnippet extends React.Component {
           'Content-Type': 'application/json'
         }
       }).then(res => res.json())
-      .then(res => {
-        if (res.status == 200) {
-          this.setState({ liked: !this.state.liked});
-          this.setState({ likeCounter: this.state.likeCounter + 1});
-        }
-      });
+        .then(res => {
+          if (res.status == 200) {
+            this.setState({ liked: !this.state.liked });
+            this.setState({ likeCounter: this.state.likeCounter + 1 });
+          }
+        });
     } else if (this.state.liked == true) {
       fetch("/api/v1/posts/" + this.props.post.id + "/unlike", {
         method: "POST",
@@ -54,12 +54,12 @@ class PostSnippet extends React.Component {
           'Content-Type': 'application/json'
         }
       }).then(res => res.json())
-      .then(res => {
-        if (res.status == 200) {
-          this.setState({ liked: !this.state.liked});
-          this.setState({ likeCounter: this.state.likeCounter - 1});
-        }
-      });
+        .then(res => {
+          if (res.status == 200) {
+            this.setState({ liked: !this.state.liked });
+            this.setState({ likeCounter: this.state.likeCounter - 1 });
+          }
+        });
     }
   }
 
@@ -67,7 +67,7 @@ class PostSnippet extends React.Component {
     return (
       <article className="media">
         <figure className="media-left">
-        <div>
+          <div>
             <p>{this.state.likeCounter}</p>
             <i className={this.state.liked ? "fa like-heart fa-heart" : "fa like-heart fa-heart-o"} onClick={this.onHeartClick.bind(this)} aria-hidden="true"></i>
           </div>
@@ -76,7 +76,7 @@ class PostSnippet extends React.Component {
           <div className="content">
             <p>
               <Link to={"/posts/" + this.props.post.id}>
-              <span className="media-title has-text-dark">{this.props.post.title}</span>
+                <span className="media-title has-text-dark">{this.props.post.title}</span>
                 <span className="media-details has-text-grey-light">Posted by <User user={this.props.post.user} /> <Moment fromNow>{this.props.post.date_created}</Moment></span>
               </Link>
             </p>
